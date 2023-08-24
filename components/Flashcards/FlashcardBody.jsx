adb18f80f16930df9b2184f1421bf42a09e26a48
Gustavo Trizotti <gustavotrizotti14@gmail.com>
2023-08-23 23:16:04 -0300

    fix: fix button flashcard color

--- components/Flashcards/FlashcardBody.jsx
+++ components/Flashcards/FlashcardBody.jsx
@@ -4,16 +4,16 @@ import React, { useState } from "react";
 const FlashcardBody = ({ selectCard }) => {
   const [card, setCard] = useState(selectCard);
   const flashcardOptions = [
-    { name: "Again", color: "#FFA0A0" },
-    { name: "Hard", color: "#FFEAA0" },
-    { name: "Good", color: "#A0FFB0" },
-    { name: "Easy", color: "#A0FFF9" },
+    { name: "Again", color: "bg-subjectRed" },
+    { name: "Hard", color: "bg-subjectYellow" },
+    { name: "Good", color: "bg-subjectGreen" },
+    { name: "Easy", color: "bg-subjectBlue" },
   ];
 
   return (
     <View className="flex bg-gray-50 w-full rounded-xl justify-center p-4">
       <View className="flex flex-2 p-2 border-b-2 border-gray-300">
-        <Text className="text-lg text-center text-secondary">
+        <Text className="text-lg font-bold text-center text-secondary">
           {card.question}
         </Text>
       </View>
@@ -21,11 +21,11 @@ const FlashcardBody = ({ selectCard }) => {
         <Text>Reveal Answer</Text>
       </View>
       <View className="flex flex-2 flex-row border-t-2 border-gray-300 py-2">
-        {flashcardOptions.map((option) => {
+        {flashcardOptions.map((option, idx) => {
           const buttonStyle = `flex flex-1 mx-1 rounded-md justify-center items-center p-3 ${option.color}`;
           return (
-            <Pressable className={buttonStyle}>
-              <Text className="">{option.name}</Text>
+            <Pressable className={buttonStyle} key={idx}>
+              <Text className="text-center text-secondary font-bold text-lg">{option.name}</Text>
             </Pressable>
           );
         })}
