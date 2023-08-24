570e613327072a62b10a4caac95b6bf04e76667f
GustavoTrizotti <gustavotrizotti14@gmail.com>
2023-08-24 10:36:47 -0300

    fix: child deck navigation

--- components/Decks/DeckListChildItem.jsx
+++ components/Decks/DeckListChildItem.jsx
@@ -1,13 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import DeckListItem from "./DeckListItem";
+import { useNavigation } from "@react-navigation/native";
 
 const DeckListChildItem = ({ parentDeck, deck, parent }) => {
+  const navigation = useNavigation();
+  
   return (
     <View>
       <DeckListItem deck={parentDeck} parent={parent} />
-      <View className="flex flex-row p-4 mb-2 mr-3 ml-8 items-center justify-between border-b-2 border-gray-100">
+      <Pressable onPress={() => {navigation.navigate("Deck", {deck: deck})}} className="flex flex-row p-4 mb-2 mr-3 ml-8 items-center justify-between border-b-2 border-gray-100">
         <View className="flex flex-row justify-center items-center gap-4">
           <Icon name="cards-variant" size={30} color="#AD6FEB" />
           <Text className="text-xl font-bold text-primary"> {deck.name} </Text>
@@ -15,7 +18,7 @@ const DeckListChildItem = ({ parentDeck, deck, parent }) => {
         <Text className="text-lg font-bold text-tertiary opacity-40">
           {deck.cards.length}
         </Text>
-      </View>
+      </Pressable>
     </View>
   );
 };
