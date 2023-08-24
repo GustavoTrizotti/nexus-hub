64d03bd61860a8319ca77650875b24e0fc76671f
GustavoTrizotti <gustavotrizotti14@gmail.com>
2023-08-24 09:53:17 -0300

    fix: fix buttons reveal answer

--- components/Flashcards/FlashcardBody.jsx
+++ components/Flashcards/FlashcardBody.jsx
@@ -1,15 +1,13 @@
 import { View, Text, Pressable } from "react-native";
 import React, { useState } from "react";
+
+import FlashcardAnswerBody from "./FlashcardAnswerBody";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const FlashcardBody = ({ selectCard }) => {
   const [card, setCard] = useState(selectCard);
-  const flashcardOptions = [
-    { name: "Again", color: "bg-subjectRed" },
-    { name: "Hard", color: "bg-subjectYellow" },
-    { name: "Good", color: "bg-subjectGreen" },
-    { name: "Easy", color: "bg-subjectBlue" },
-  ];
+  const [isAnswer, setIsAnswer] = useState(false);
+  /* TODO - GET NEXT CARD AFTER RESPONSE */
 
   return (
     <View className="flex bg-gray-100 rounded-xl justify-between h-full m-2 p-4 px-6">
@@ -18,24 +16,24 @@ const FlashcardBody = ({ selectCard }) => {
           {card.question}
         </Text>
       </View>
-      <View className="flex p-2 h-fit justify-center items-center">
-        <Pressable className="flex flex-row" onPress={() => {console.log("Teste")}}>
-          <Text className="text-center text-xl">Reveal Answer</Text>
-          <Icon name="chevron-right" size={30}/>
-        </Pressable>
-      </View>
-      <View className="flex flex-row border-t-2 border-gray-300 py-4">
-        {flashcardOptions.map((option, idx) => {
-          const buttonStyle = `flex flex-1 mx-1 rounded-md justify-center items-center p-2 ${option.color}`;
-          return (
-            <Pressable className={buttonStyle} key={idx}>
-              <Text className="text-center text-secondary font-bold text-lg">
-                {option.name}
-              </Text>
+      {isAnswer ? (
+        <FlashcardAnswerBody sCard={card} />
+      ) : (
+        <View>
+          <View className="flex p-2 h-fit justify-center items-center"></View>
+          <View className="flex flex-row border-t-2 border-gray-300 py-4 justify-center items-center">
+            <Pressable
+              className="flex flex-row"
+              onPress={() => {
+                setIsAnswer(true);
+              }}
+            >
+              <Text className="text-center text-xl">Reveal Answer</Text>
+              <Icon name="chevron-right" size={30} />
             </Pressable>
-          );
-        })}
-      </View>
+          </View>
+        </View>
+      )}
     </View>
   );
 };
