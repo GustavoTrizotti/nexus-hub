54453b57a65cb89a685e6afe63d2e8454f32fa03
GustavoTrizotti <gustavotrizotti14@gmail.com>
2023-09-05 12:20:19 -0300

    fix: main header font

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -3,10 +3,20 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
 const MainHeader = ({ title, search }) => {
+
+  const [fontsLoaded] = useFonts({
+    'Sora': require('../assets/fonts/Sora/Sora-Regular.ttf')
+  });
+
   const navigation = useNavigation();
   const route = useRoute();
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <View className="bg-white p-6 flex flex-row items-center justify-between">
@@ -19,11 +29,11 @@ const MainHeader = ({ title, search }) => {
         </TouchableWithoutFeedback>
       )}
       {title != null ? (
-        <Text className="text-primary text-center text-2xl font-bold">
+        <Text className="text-primary text-center text-2xl font-bold" style={{fontFamily: 'Sora'}}>
           {title}
         </Text>
       ) : (
-        <Text className="text-primary text-center uppercase text-2xl font-bold">
+        <Text className="text-primary text-center uppercase text-2xl font-bold" style={{fontFamily: 'Sora'}}>
           {route.name}
         </Text>
       )}
