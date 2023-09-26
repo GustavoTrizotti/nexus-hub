1c977ca4eb823e78a0de0ab606dafb1762e71a8d
GustavoTrizotti <gustavotrizotti14@gmail.com>
2023-09-26 10:44:26 -0300

    feaT [develop]: swipe to delete start

--- components/Subjects/SubjectCard.jsx
+++ components/Subjects/SubjectCard.jsx
@@ -1,33 +1,77 @@
 import { View, Text } from "react-native";
 import React from "react";
-import scheme from "../../utils/subjectColorScheme";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import SubjectCardDifficulty from "./SubjectCardDifficulty";
 import { changeTextColor } from "../../utils/textColorHex";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 const SubjectCard = ({ subject }) => {
-  const bgColor = subject.color
+  const bgColor = subject.color;
   const colors = {
     color: subject.color,
     dark: "#FFF",
-    light: "#2E2E2E"
-  }
+    light: "#2E2E2E",
+  };
 
-  const color = changeTextColor(colors.color, colors.dark, colors.light)
+  const color = changeTextColor(colors.color, colors.dark, colors.light);
+  const translateX = useSharedValue(0);
+
+  const panGesture = useAnimatedGestureHandler({
+    onActive: (event) => {
+      translateX.value = event.translationX;
+    },
+    onEnd: (event) => {
+      translateX.value = withTiming(0);
+    }, 
+  });
+
+  const reanimatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+    ],
+  }));
 
   return (
-    <View
-      className="flex px-4 py-12 w-full justify-center items-center my-4 rounded-lg"
-      style={{backgroundColor: bgColor}}
-    >
-      <Text className="text-2xl text-tertiary font-bold mb-8 text-center" style={{color: color}}>
-        {subject.name}
-      </Text>
-      <View className="flex flex-row items-center justify-center">
-        <Icon name="fire" size={30} color={color}/>
-        <SubjectCardDifficulty diff={subject.difficulty} color={color}/>
+    <GestureHandlerRootView className="flex w-full flex-row my-4">
+      <View style={{
+        position: 'absolute',
+        right: '1%',
+        display: 'flex',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+         <Icon name="trash-can" size={60} color="#f7473e"/>
       </View>
-    </View>
+      <PanGestureHandler onGestureEvent={panGesture}>
+        <Animated.View
+          className="w-full justify-center items-center px-4 py-12 rounded-lg"
+          style={[reanimatedStyle, { backgroundColor: bgColor }]}
+        >
+          <Text
+            className="text-2xl text-tertiary font-bold mb-8 text-center"
+            style={{ color: color }}
+          >
+            {subject.name}
+          </Text>
+          <View className="flex flex-row items-center justify-center">
+            <Icon name="fire" size={30} color={color} />
+            <SubjectCardDifficulty diff={subject.difficulty} color={color} />
+          </View>
+        </Animated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
 };
 
